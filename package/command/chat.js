ec50292678a6a7c663fd325286440756618068b9
codeacme17 <vir_kings@qq.com>
2023-03-01 20:32:39 +0800

    update: chat command code

--- package/command/chat.js
+++ package/command/chat.js
@@ -4,7 +4,7 @@ const chalk = require("chalk")
 const Load = require("../utils/Load")
 const History = require("../utils/History")
 const { COLORS } = require("../utils/config")
-const { clear, log, error } = require("../utils/log")
+const { clear, log } = require("../utils/log")
 const { chat, explainCode } = require("../utils/api")
 
 NODE_REPL_HISTORY = ""
@@ -16,23 +16,12 @@ const history = new History()
  */
 module.exports = () => {
   history.init()
+
+  startChatLog()
   startREPL()
 }
 
-/** Starts a new REPL session to chat with GPT-3.
-    @returns {void}
- */
 function startREPL() {
-  clear()
-  startChatLog()
-
-  log(
-    `${chalk.hex(COLORS.GRAY)(
-      "start at: " + new Date().toLocaleDateString(),
-      new Date().toLocaleTimeString()
-    )}\n`
-  )
-
   repl.start({
     prompt: `${chalk.hex(COLORS.GREEN)("Question: ")}\n`,
     eval,
@@ -40,22 +29,17 @@ function startREPL() {
   })
 }
 
-/** Evaluates the user's command and retrieves a response from the OpenAI API.
-    @param {string} cmd - The user's command
-    @param {Object} context - The context object passed to the REPL session
-    @param {string} filename - The name of the file being executed
-    @param {Function} cb - The callback function to return the response to the REPL session
-    @returns {void}
- */
 async function eval(cmd, context, filename, cb) {
   const formatedCmd = formatCmd(cmd)
   const commendType = chatCommand(formatedCmd)
 
   if (!formatedCmd || load.loading) return
+
   load.start()
   const res = await requestOpenai(cmd, commendType)
   history.write(formatedCmd + "\n", "QUESTION")
   load.end()
+
   cb(null, res)
 }
 
@@ -64,19 +48,11 @@ function writer(output) {
   return `${chalk.hex(COLORS.YELLOW)("Answer: ")}\n${output}\n`
 }
 
-/** Formats the user's command by removing any trailing newline characters.
-    @param {string} cmd - The user's command
-    @returns {string} The formatted command
- */
 function formatCmd(cmd) {
   const index = cmd.lastIndexOf("\n")
   return cmd.substring(0, index)
 }
 
-/** Determines the type of command being executed based on its prefix.
-    @param {string} cmd - The user's command
-    @returns {string} The type of command being executed
- */
 function chatCommand(cmd) {
   if (cmd === "/") process.exit(1)
   if (cmd === "clear") {
@@ -84,13 +60,9 @@ function chatCommand(cmd) {
     process.exit(1)
   }
   if (/^\/code/.test(cmd)) return "code"
+  if (/^\/note/.test(cmd)) return "note"
 }
 
-/** Sends a request to the OpenAI API to retrieve a response to the user's command.
-    @param {string} cmd - The user's command
-    @param {string} type - The type of command being executed
-    @returns {Promise<string>} The response to the user's command
- */
 async function requestOpenai(cmd, type) {
   let res
   switch (type) {
@@ -106,7 +78,14 @@ async function requestOpenai(cmd, type) {
 }
 
 function startChatLog() {
+  clear()
   log()
   log(`${chalk.hex(COLORS.PURPLE)("🤖 CHAT WITH GPT MODEL HERE...")}`)
   log()
+  log(
+    `${chalk.hex(COLORS.GRAY)(
+      "start at: " + new Date().toLocaleDateString(),
+      new Date().toLocaleTimeString()
+    )}\n`
+  )
 }
--- package/utils/questions.js
+++ package/utils/questions.js
@@ -1,5 +1,4 @@
 const inquirer = require("inquirer")
-const chalk = require("chalk")
 
 const _confirmClearHistory = () =>
   inquirer.prompt([
