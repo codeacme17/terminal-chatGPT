867c0403e968094190a1153d791df989f8fb8b10
codeacme17 <vir_kings@qq.com>
2023-03-01 19:48:06 +0800

    update: terminal logo content

--- package/utils/generate-cfonts.js
+++ package/utils/generate-cfonts.js
@@ -1,19 +1,21 @@
 const cfonts = require("cfonts")
 
 function generate() {
-  return cfonts.render("leyoonafr.", {
-    font: "tiny", // define the font face
-    align: "left", // define text alignment
-    colors: ["system"], // define all colors
-    background: "transparent", // define the background color, you can also use `backgroundColor` here as key
-    letterSpacing: 2, // define letter spacing
-    lineHeight: 1, // define the line height
-    space: true, // define if the output text should have empty lines on top and on the bottom
-    maxLength: "20", // define how many character can be on one line
-    gradient: ["white", "#1e1e1e"], // define your two gradient colors
-    independentGradient: false, // define if you want to recalculate the gradient for each new line
-    transitionGradient: false, // define if this is a transition between colors directly
-    env: "node", // define the environment cfonts is being executed in
+  const content = `|  1llest Group|  TERMINAL-GPT|`
+
+  return cfonts.render(content, {
+    font: "console",
+    align: "left",
+    colors: ["system"],
+    background: "transparent",
+    letterSpacing: 1,
+    lineHeight: 1,
+    space: false,
+    maxLength: "20",
+    gradient: ["white", "#1e1e1e"],
+    independentGradient: false,
+    transitionGradient: false,
+    env: "node",
   })
 }
 
