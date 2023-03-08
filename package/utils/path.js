1b8470fb3e8df4bf717bf8e8bc06a47be38e145b
codeacme17 <vir_kings@qq.com>
2023-03-08 19:00:24 +0800

    chore: update `path` util

--- package/utils/path.js
+++ package/utils/path.js
@@ -1,7 +1,9 @@
 const path = require("path")
 
 const API_FILE = path.resolve(__dirname, "../../KEY.json")
+const HISTORY_FILE = path.resolve(__dirname, "../../chat_history.txt")
 
 module.exports = {
   API_FILE,
+  HISTORY_FILE,
 }
