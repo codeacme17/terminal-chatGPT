78408221677df5f9b0fc24355aee26ed7c5543c1
codeacme17 <vir_kings@qq.com>
2023-03-08 15:12:18 +0800

    feat: add language highlight feature

--- package/utils/CodeBoxer.js
+++ package/utils/CodeBoxer.js
@@ -1,38 +1,46 @@
 const boxen = require("boxen")
+const highlight = require("cli-highlight").highlight
 
 class CodeBoxer {
   constructor() {
     this.codeRegex = /```(\w+)?\n([\s\S]*?)\n```/g
+    this.boxenOptions = {
+      padding: {
+        top: 1,
+        right: 10,
+        bottom: 1,
+        left: 5,
+      },
+      dimBorder: true,
+      backgroundColor: "#333333",
+      borderStyle: {
+        topLeft: " ",
+        topRight: " ",
+        bottomLeft: " ",
+        bottomRight: " ",
+        top: " ",
+        bottom: " ",
+        left: " ",
+        right: " ",
+        vertical: " ",
+        horizontal: " ",
+      },
+      fullscreen: (width) => [width - 2],
+      float: "center",
+    }
   }
 
   boxify(text) {
     return text.replaceAll(this.codeRegex, (match, language, code) => {
-      const boxenOptions = {
-        padding: {
-          top: 1,
-          right: 10,
-          bottom: 1,
-          left: 5,
-        },
-        dimBorder: true,
-        backgroundColor: "#333333",
-        borderStyle: {
-          topLeft: " ",
-          topRight: " ",
-          bottomLeft: " ",
-          bottomRight: " ",
-          top: " ",
-          bottom: " ",
-          left: " ",
-          right: " ",
-          vertical: " ",
-          horizontal: " ",
-        },
-        fullscreen: (width) => [width - 2],
-        float: "center",
-      }
+      const highlighted = this.highlighty(code, language)
+      return boxen(highlighted, this.boxenOptions)
+    })
+  }
 
-      return boxen(code, boxenOptions)
+  highlighty(code, language) {
+    return highlight(code, {
+      language: language ? language : "javascript",
+      ignoreIllegals: true,
     })
   }
 }
