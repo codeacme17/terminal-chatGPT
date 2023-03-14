22235f01241536bd66ca6ab483baacda7709f1bf
codeacme17 <vir_kings@qq.com>
2023-03-14 19:35:31 +0800

    refactor: remove other GPT models APIs

--- package/utils/apis.js
+++ package/utils/apis.js
@@ -22,63 +22,9 @@ async function Turbo(cache) {
   }
 }
 
-// chat model api
-async function DavinciChat(prompt) {
-  const openai = initConfiguration()
-
-  try {
-    const response = await openai.createCompletion(
-      {
-        model: "text-davinci-003",
-        prompt: `Q: ${prompt} \n`,
-        temperature: 0,
-        max_tokens: 200,
-        top_p: 1,
-        frequency_penalty: 1,
-        presence_penalty: 1,
-        stop: ["Q: "],
-      },
-      {
-        timeout: 30 * 1000,
-      }
-    )
-    const res = response.data.choices[0].text.trim()
-    return formatResponse(res)
-  } catch (err) {
-    errorResponse(err)
-  }
-}
-
-// explain code model api
-async function DavinciCode(prompt) {
-  const openai = initConfiguration()
-
-  try {
-    const response = await openai.createCompletion(
-      {
-        model: "code-davinci-002",
-        prompt: `code: ${prompt} \n`,
-        temperature: 0,
-        max_tokens: 200,
-        top_p: 1.0,
-        frequency_penalty: 0.0,
-        presence_penalty: 0.0,
-        stop: ["code: "],
-      },
-      {
-        timeout: 30 * 1000,
-      }
-    )
-    return response.data.choices[0].text.trim()
-  } catch (err) {
-    errorResponse(err)
-  }
-}
 
 module.exports = {
-  Turbo,
-  DavinciChat,
-  DavinciCode,
+  Turbo
 }
 
 function initConfiguration() {
@@ -103,9 +49,3 @@ function errorResponse(err) {
   else error(err)
 }
 
-function formatResponse(res) {
-  if (res.startsWith("\n\n")) res = res.substring(2)
-  if (res.startsWith("A: ")) res = res.substring(3)
-  if (res.startsWith("Answer: ")) res = res.substring(8)
-  return res
-}
